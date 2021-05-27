e6653a94850c847c396eb63235e5b6a3d5fd77a5
Lovepreet Singh <LS052@atcltd.atc.org.nz>
2021-05-27 13:52:48 +1200

    update

--- Business_Layer/Clothes.cs
+++ Business_Layer/Clothes.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading.Tasks; // add libraries
 
 namespace API_project.Business_Layer
 {
-    public class Clothes
+    public class Clothes // class
     {
         [Key]
-        public int Id{ get; set; }  // id
+        public int Id{ get; set; }  // id primery key
         public string Brand { get; set; }  // brand 
         
         public string Name { get; set; }// brand name
-        public string Size { get; set; }
-        public int Price { get; set; }
-        public int Quntity { get; set; }
+        public string Size { get; set; } // size
+        public int Price { get; set; }// price
+        public int Quntity { get; set; }// quntity
     }
 }
